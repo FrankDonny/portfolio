12fb15309e92dfea69a64b4875f1f4d140cc173b
frankdonny <arkohfrank1@gmail.com>
2023-03-06 00:07:27 +0000

    update

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,18 @@
-const menu = document.querySelector('.menu-icon');
-menu.addEventListener('click', ()=>{
-    const menuList = document.querySelector('#navbar');
-    if (menuList.style.display === 'block'){
-        menuList.style.display = 'none';
-    } else {
-        menuList.style.display = 'block';
-    }
+const idArray = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+idArray.forEach((ele) => {
+    const lang = document.querySelector(`#${ele}`);
+    lang.addEventListener('mouseover', () => {
+        const theId = document.querySelector(`#${ele} .names`)
+        theId.style.transform = 
+        theId.style.display = 'none';
+        const namesDesc = document.querySelector(`#${ele} .namesDesc`)
+        namesDesc.style.display = 'grid';
+    })
+
+    lang.addEventListener('mouseout', ()=>{
+        const theId = document.querySelector(`#${ele} .names`)
+        theId.style.display = 'block';
+        const namesDesc = document.querySelector(`#${ele} .namesDesc`)
+        namesDesc.style.display = 'none';
+    })
 })
